6481a987f8ed00584fd0ba71466e4cf3b495ace4
hack-feng <1150640979@qq.com>
2019-12-21 11:50:17 +0800

    add Springboot project

---
+++ src/main/java/com/maple/javautil/JavaUtilApplication.java
@@ -0,0 +1,17 @@
+package com.maple.javautil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 的启动类
+ * @author Maple
+ */
+@SpringBootApplication
+public class JavaUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUtilApplication.class, args);
+    }
+
+}
